191fd111cddb91df03b13650422dea63c7c80ea6
BriWS <brightwiths@gmail.com>
2021-10-24 14:40:07 +0300

    lesson 6 after lecture

--- src/lessons/lesson6/lesson6.ts
+++ src/lessons/lesson6/lesson6.ts
@@ -1,3 +1,5 @@
+import {log} from "util";
+
 console.log('Lesson 6');
 
 // Class
@@ -7,6 +9,245 @@ console.log('Lesson 6');
 // https://www.typescriptlang.org/docs/handbook/classes.html
 // + https://www.youtube.com/watch?v=BASquaxab_w
 // + https://www.youtube.com/watch?v=uLY9GXGMXaA
+
+// function Test() {
+//     this.name = name
+//     this.age = age
+//     // this.sayHi = function () {}
+// }
+// Test.prototype.sayHi = function () {}
+
+// class Test {
+//     name: string
+//     age: number
+//     // testParam: any
+//     // sayHi: Function
+//     // arrow: Function
+//     // bindSayHi: Function
+//
+//     constructor(name: string, age: number, param: any) {
+//         this.name = name
+//         this.age = age
+//         //this.sayHi = function () {}
+//         //this.arrow = () => {}
+//         //this.bindSayHi = this.sayHi.bind(this)
+//         // this.testParam = param
+//     }
+//
+//     sayHi() {
+//         console.log(this.name)
+//     }
+//
+//     arrow = () => {}
+//
+// }
+//
+// // @ts-ignore
+// Test.prototype.testParam = 10
+//
+// let obj = new Test('Briws',34, 0)
+// let obj2 = new Test('Briws',34, 20)
+// let obj3 = new Test('Briws',34, 50)
+// console.log(obj)
+// console.log(obj2)
+// console.log(obj3)
+
+// console.dir(function Test() {})
+// console.dir(class Test{})
+
+// class Test {
+//     name: string
+//
+//     constructor(name: string) {
+//         this.name = name
+//     }
+// }
+//
+// class Test2 extends Test {
+//     age: number
+//
+//     constructor(name: string, age: number) {
+//         super(name) // фактически ссылка на родительский конструктор. Он никогда не теряет контекст
+//         this.age = age
+//     }
+// }
+//
+// let obj = new Test2('Yo',10)
+// console.log(obj)
+
+// class Test {
+//     sayHi() {}
+//     // когда нет конструктора, то от создаётся автоматически под капотом
+// }
+//
+// class Test2 extends Test {
+//     // name: string
+//     // constructor(name: string) {
+//     //     super();
+//     //     this.name = name
+//     // }
+//     sayBye() {}
+// }
+//
+// let obj = new Test2()
+// console.log(obj)
+
+// class Test {
+//     name?: string
+//     constructor(name?: string) {
+//         this.name = name
+//     }
+//     sayHi() {}
+// }
+// let testObj = new Test('yo')
+//
+// class Test2 extends Test {
+//     // name: string
+//     age: number
+//     constructor(name: string, age: number) {
+//         super(name);
+//         // this.name = name
+//         this.age = age
+//     }
+//     sayBye() {}
+// }
+
+// interface ITest {
+//     name: string,
+//     age: number,
+//     sayHi: Function,
+// }
+// interface ITest2 {
+//     name: string,
+//     city: string
+// }
+//
+// class Test implements ITest, ITest2 {
+//     name: string
+//     age: number
+//     city: string
+//
+//     constructor(name: string, age: number, city: string) {
+//         this.name = name
+//         this.age = age
+//         this.city = city
+//     }
+//
+//     sayHi() {}
+//
+//     sayBye() {}
+// }
+
+// class Test {
+//     name: string
+//
+//     constructor(name: string) {
+//         this.name = name
+//     }
+//
+//     sayName() {
+//         console.log(this.name)
+//     }
+//     testArrow = () => {
+//         console.log('test')
+//     }
+// }
+//
+// class Test2 extends Test {
+//     constructor(name: string) {
+//         super(name);
+//     }
+//
+//     sayName() {
+//         console.log('yo yo yo')
+//         super.sayName()
+//     }
+//
+//     testArrow = () => {
+//         console.log('yo')
+//         // @ts-ignore
+//         super.testArrow()
+//         // @ts-ignore
+//         // console.log(super.testParam)
+//         console.log(this.testParam)
+//     }
+// }
+//
+// Test.prototype.testArrow = () => {
+//     console.log('Prototype test')
+// }
+// // @ts-ignore
+// Test.prototype.testParam = 0;
+//
+// let obj = new Test2('Briws')
+// console.log(obj)
+// // obj.sayName()
+// obj.testArrow()
+
+// class Test {
+//     fullData: string
+//
+//     // public под капотом делает типизацию и присвоение. Упрощает типизацию.
+//     constructor(public name: string, public age: number) {
+//         this.fullData = this.name + ' -  ' + this.age
+//     }
+//
+//
+// }
+//
+// let obj = new Test('Briws', 33)
+// console.log(obj)
+
+// class Test {
+//     private testParam = 10
+//
+//     constructor(public name: string, public age: number) { }
+//
+//     sayTestParam() {
+//         console.log(this.testParam)
+//     }
+//
+// }
+//
+// let obj = new Test('Briws', 33)
+
+// console.log(obj)
+// console.log(obj.testParam)
+
+// class Test {
+//     static testParam = 10
+//     constructor(public name: string) {
+//     }
+//     sayName() {
+//         console.log(this.name)
+//     }
+//     static sayNames(arr: Array<Test>) {
+//         arr.forEach(console.log)
+//     }
+// }
+//
+// console.dir(Test)
+
+class Test {
+    private testParam = 0;
+
+    get param() {
+        return this.testParam
+    }
+
+    set param(value) {
+        this.testParam = value
+    }
+
+    testMethod() {
+        console.log(this.testParam)
+    }
+}
+
+let obj = new Test()
+obj.param = 50
+console.log(obj.param)
+
 
 // Task 01
 // Создайте структуру с именем student, содержащую поля: имя и фамилия, номер группы, успеваемость (массив из пяти элементов).
